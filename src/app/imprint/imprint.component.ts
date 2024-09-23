768d1930ebd1bc2b6cc9e16b33839d68a79df50b
EmiKilic <emirhan_kilic@outlook.de>
2024-09-23 22:25:31 +0200

    final push

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -2,19 +2,15 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/header/header.component';
 import { FooterComponent } from '../shared/footer/footer.component';
 import { HeaderFooterService } from '../shared/header-footer.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, RouterLink],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss',
 })
 export class ImprintComponent {
-  constructor(private sharedService: HeaderFooterService) {}
 
-
-  goBack() {
-    history.back();
-  }
 }
--- src/app/polic/polic.component.ts
+++ src/app/polic/polic.component.ts
@@ -1,18 +1,15 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/header/header.component';
 import { FooterComponent } from '../shared/footer/footer.component';
-import { HeaderFooterService } from '../shared/header-footer.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-polic',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, RouterLink],
   templateUrl: './polic.component.html',
   styleUrl: './polic.component.scss'
 })
 export class PolicComponent {
-  constructor(private sharedService: HeaderFooterService) {}
-  goBack() {
-    history.back();
-  }
+
 }
